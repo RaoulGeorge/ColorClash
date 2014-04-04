597d1f72cfc48b65c597b1a052d6c53ee6ff6cf6
Raoul George <raoulgegorge@gmail.com>
2014-04-04 15:29:02 +0530

    changing to fg and bg. removing the 2nd output box

--- apps/startup/viewmodel.js
+++ apps/startup/viewmodel.js
@@ -5,14 +5,13 @@ define(function (require) {
 
     function viewmodel() {
         console.log('viewmodel constructor');
-        leftBackground = ko.observable();
-        rightBackground = ko.observable();
+        bgColor = ko.observable('black');
+        fgColor = ko.observable('white');
     }
 
     //var base = Object.inherit(KnockoutViewModel, SummaryHealthViewModel);
 
     viewmodel.prototype.activate = function () {
-        console.log('viewmodel activate');
         $('body').append(view);
         ko.applyBindings(this);
     };
