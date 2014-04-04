a6dff021c9ef9feb15740655f7b30ef0cab6ff45
Raoul George <raoulgeorge@gmail.com>
2014-04-04 18:40:04 +0530

    wiring up the compliance values

--- apps/startup/viewmodel.js
+++ apps/startup/viewmodel.js
@@ -8,6 +8,7 @@ define(function (require) {
         console.log('viewmodel constructor');
         this.bgColor = ko.observable('black');
         this.fgColor = ko.observable('white');
+        this.compliance1 = ko.observable();
         this.bgRGBValue = '';
         this.fgRGBValue = '';
         this.color = new color();
@@ -18,20 +19,21 @@ define(function (require) {
     viewmodel.prototype.activate = function () {
         var self = this;
         $('body').append(view);
-        ko.applyBindings(this);
-
+        
+        //this.compliance1 = 'we';
         this.bgColor.subscribe(function(newValue) {
             var hexValue = self.color.nameToHex(newValue);
             self.bgRGBValue = self.color.hexToRGB(hexValue);
-            checkCompliance(self);
+            self.checkCompliance();
         });
 
         this.fgColor.subscribe(function(newValue) {
             var hexValue = self.color.nameToHex(newValue);
             self.fgRGBValue = self.color.hexToRGB(hexValue);
-            checkCompliance(self);
+            self.checkCompliance();
         });
 
+ko.applyBindings(this);
 
     };
 
@@ -40,12 +42,26 @@ define(function (require) {
 
     };
 
-    function checkCompliance(self){
+    viewmodel.prototype.checkCompliance = function(){
+        var self = this;
+        console.log(self);
         var l1 = self.color.getLuminosity(self.bgRGBValue);
         var l2 = self.color.getLuminosity(self.fgRGBValue);
         console.log(self.fgRGBValue);
 
-        console.log('contrast =' + self.color.getContrast(l1, l2));
+        self.contrast = self.color.getContrast(l1, l2);
+        
+        if(self.contrast < 5) {
+            self.compliance1('bad');
+        } else if(self.contrast < 10) {
+            self.compliance1('could be better');
+        } else if(self.contrast < 13) {
+            self.compliance1('OK');
+        } else if(self.contrast < 18) {
+            self.complianc1('Good');
+        } else {
+            self.compliance1('Great');
+        }
 
     }
 
